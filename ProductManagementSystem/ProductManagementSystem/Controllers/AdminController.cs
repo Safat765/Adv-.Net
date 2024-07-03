0383118fc2c83daacafdaba09c9282a85a19e7c0
Safat765 <88162223+Safat765@users.noreply.github.com>
2024-07-04 02:25:45 +0600

    PMSDemo

---
+++ ProductManagementSystem/ProductManagementSystem/Controllers/AdminController.cs
@@ -0,0 +1,79 @@
+﻿using ProductManagementSystem.DTO;
+using ProductManagementSystem.Auth;
+using ProductManagementSystem.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+
+namespace ProductManagementSystem.Controllers
+{
+    [Admin]
+    public class AdminController : Controller
+    {
+        PMSDemoEntities db = new PMSDemoEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session["user"] = null;
+
+            TempData["msg"] = "Logout Successfully";
+
+            return RedirectToAction("Index", "Login");
+        }
+        public ActionResult Order()
+        {
+            var data = db.Orders.ToList();
+            return View(OrderController.Convert(data));
+        }
+        public ActionResult OrderDetails(int id)
+        {
+            var data = (from od in db.OrderProducts
+                        where od.OId == id
+                        select od).ToList();
+
+            //return View(data);
+
+            var con = new MapperConfiguration(cfg => { 
+                cfg.CreateMap<OrderProduct, OrderProductDTO>(); 
+                cfg.CreateMap<Product, ProductDTO>(); 
+            });
+            var mapper = new Mapper(con);
+            var pd = mapper.Map<List<OrderProductDTO>>(data);
+
+            return View(pd);
+        }
+        public ActionResult DeclineOrder(int id)
+        {
+            var demo = db.Orders.Find(id);
+            demo.Status = "Decliened";
+            db.SaveChanges();
+            TempData["msg"] = "Order id " + id + " has been decliened";
+
+            return RedirectToAction("Order");
+        }
+        public ActionResult AcceptOrder(int id)
+        {
+            var demo = db.Orders.Find(id);
+            var orderProduct = demo.OrderProducts;
+
+            foreach (var item in orderProduct)
+            {
+                item.Product.Qty -= item.Qty;
+            }
+
+            demo.Status = "Processing";
+            db.SaveChanges();
+            TempData["msg"] = "Order id " + id + " processing";
+
+            return RedirectToAction("Order");
+        }
+
+    }
+}
---
+++ ProductManagementSystem/ProductManagementSystem/Controllers/OrderController.cs
@@ -0,0 +1,119 @@
+﻿using ProductManagementSystem.Auth;
+using ProductManagementSystem.DTO;
+using ProductManagementSystem.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductManagementSystem.Controllers
+{
+    [Customer]
+    // authorization overcome korar jonno [AllowAnonymous] use kora hoi
+    public class OrderController : Controller
+    {
+        // GET: Order
+
+        PMSDemoEntities db = new PMSDemoEntities();
+        public ActionResult Index()
+        {
+            var demo = db.Products.ToList();
+            return View(ProductController.Convert(demo));
+        }
+        public ActionResult AddToCart(int id)
+        {
+            var demo = db.Products.Find(id);
+            var con = ProductController.Convert(demo);
+            
+            con.Qty = 1;
+
+            if (Session["cart"] == null)
+            {
+                var cart = new List<ProductDTO>(); // boxing
+                cart.Add(con);
+                Session["cart"] = cart;
+            }
+            else
+            {
+                var cart = Session["cart"];
+                var data = (List<ProductDTO>)cart; // unboxing
+                data.Add(con);
+                Session["cart"] = data;
+            }
+            
+            TempData["msg"] = con.Name + " Added on cart Successfully";
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult Cart()
+        {
+            var demo = Session["cart"];
+            if(demo == null)
+            {
+                TempData["msg"] = "Cart is empty";
+                return RedirectToAction("Index");
+            }
+            var data = (List<ProductDTO>)demo; // unboxing
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult PlaceOrder(decimal Total)
+        {
+            var order = new Order();
+            order.OrderDate = DateTime.Now;
+            order.Status = "Ordered";
+            order.TotalAmount = Total;
+            order.UId = ((User)Session["user"]).Id;
+            db.Orders.Add(order);
+            db.SaveChanges();
+
+            var cart = (List<ProductDTO>)Session["cart"];
+
+            
+            foreach(var item in cart)
+            {
+                var op = new OrderProduct();
+                op.Qty = item.Qty;
+                op.PId = item.Id;
+                op.UnitPrice = item.Price;
+                op.OId = order.Id;
+                db.OrderProducts.Add(op);
+            }
+            db.SaveChanges();
+
+            Session["cart"] = null;
+            TempData["msg"] = "Order has benn placed successfully";
+            return RedirectToAction("Index");
+        }
+        public static OrderDTO Convert(Order o)
+        {
+            return new OrderDTO(){
+                Id = o.Id,
+                Status = o.Status,
+                TotalAmount = o.TotalAmount,
+                OrderDate = o.OrderDate,
+                UId = o.UId
+            };
+        }
+        public static Order Convert(OrderDTO o)
+        {
+            return new Order(){
+                Id = o.Id,
+                Status = o.Status,
+                TotalAmount = o.TotalAmount,
+                OrderDate = o.OrderDate,
+                UId = o.UId
+            };
+        }
+        public static List<OrderDTO> Convert(List<Order> o)
+        {
+            var list = new List<OrderDTO>();
+            foreach (var item in o)
+            {
+                list.Add(Convert(item));
+            }
+            return list;
+        }
+    }
+}
