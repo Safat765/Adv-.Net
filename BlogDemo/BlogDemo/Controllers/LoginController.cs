afb64a8b641c13e4a53147f5f929cf93f4dc1fe9
Safat765 <88162223+Safat765@users.noreply.github.com>
2024-07-04 13:31:30 +0600

    BlogDemo

---
+++ BlogDemo/BlogDemo/Controllers/LoginController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogDemo.DTO;
+using BlogDemo.EF;
+
+namespace BlogDemo.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        BlogSiteEntities db = new BlogSiteEntities();
+
+        public static UserDTO Convert(User u)
+        {
+            return new UserDTO(){
+                Name = u.Name,
+                Password = u.Password
+            };
+        }
+        public static User Convert(UserDTO u)
+        {
+            return new User(){
+                Name = u.Name,
+                Password = u.Password
+            };
+        }
+        public static List<UserDTO> Convert(List<User> u)
+        {
+            var list = new List<UserDTO>();
+            foreach(var item in u)
+            {
+                list.Add(Convert(item));
+            }
+            return list;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(UserDTO u)
+        {
+            if (ModelState.IsValid)
+            {
+                var demo = (from user in db.Users
+                            where user.Name.Equals(u.Name) &&
+                            user.Password.Equals(u.Password)
+                            select user).SingleOrDefault();
+                if (demo == null)
+                {
+                    TempData["msg"] = "User name or password missmatch";
+                    return RedirectToAction("Index");
+                }
+                Session["user"] = demo;
+                TempData["msg"] = "Login Successfully";
+                return RedirectToAction("Index", "Dashboard");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(UserDTO u)
+        {
+            if (ModelState.IsValid)
+            {
+                var demo = Convert(u);
+                db.Users.Add(demo);
+                db.SaveChanges();
+                TempData["msg"] = "Registration Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(u);
+        }
+
+    }
+}
---
+++ BlogDemo/BlogDemo/DTO/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogDemo.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Username required!")]
+        [StringLength(25, MinimumLength = 5)]
+        [RegularExpression(@"[^\s]+", ErrorMessage = "Username cannot have any space")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Password required!")]
+        [StringLength(50, MinimumLength = 8)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z\s]).{8,50}$", ErrorMessage = "Password must be between 8 and 50 characters in length, contain at least 1 capital letter, 1 small letter, 1 special character (no spaces), and 1 number.")]
+        public string Password { get; set; }
+    }
+}
